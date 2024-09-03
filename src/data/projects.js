2d8396d0b785ec7b60c428d77818f1c2520cdb46
Rafif Athallah <rafifathallah99@gmail.com>
2024-09-03 22:39:26 +0700

    feat: add new design and section data

---
+++ src/data/projects.js
@@ -0,0 +1,23 @@
+export const projectsList = [
+    {
+        id: '1',
+        title: 'Manual Parking System',
+        desc: 'A streamlined manual parking system built with native PHP for efficient space management.',
+        techs: ['PHP', 'MySQL', 'Bootstrap'],
+        github: 'https://github.com/zangetsuuuu/manual-parking-system',
+    },
+    {
+        id: '2',
+        title: 'Toilet Checklist System',
+        desc: 'An intuitive checklist system for maintaining restroom cleanliness, powered by native PHP.',
+        techs: ['PHP', 'MySQL', 'Bootstrap'],
+        github: 'https://github.com/zangetsuuuu/toilet-checklist-system',
+    },
+    {
+        id: '3',
+        title: 'Simple Online Store',
+        desc: 'A lightweight online store platform offering seamless online shopping with Faspay integration for easy payments—no shipping required.',
+        techs: ['CodeIgniter4', 'MySQL', 'TailwindCSS', 'Flowbite'],
+        github: 'https://github.com/zangetsuuuu/ci4-online-store',
+    },
+];
