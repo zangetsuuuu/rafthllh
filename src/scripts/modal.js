48879817cb5113b6e05bc2d44ab6f9dcf95a41e3
Rafif Athallah <rafifathallah99@gmail.com>
2024-09-03 22:43:00 +0700

    fix: make some changes

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,36 +1,49 @@
-document.querySelectorAll('#modal-btn').forEach((modal) => {
-    modal.addEventListener('click', (e) => {
-        const target = e.target;
-        const targetSrc = target.getAttribute('design-id');
-        const targetTitle = target.getAttribute('design-title');
+function openModal(target) {
+    const targetSrc = target.getAttribute('design-id');
+    const targetTitle = target.getAttribute('design-title');
 
-        if (targetSrc && targetTitle) {
-            const modalImage = document.getElementById('modal-image');
+    if (targetSrc && targetTitle) {
+        const modalImage = document.getElementById('modal-image');
+        if (modalImage) {
             modalImage.src = `/images/gallery/${targetSrc}.webp`;
             modalImage.alt = targetTitle;
         }
+    }
 
-        const modal = document.getElementById('modal');
-        if (modal) {
-            modal.classList.replace('hidden', 'flex');
-            setTimeout(() => {
-                modal.classList.remove('opacity-0');
-                modal.classList.add('opacity-100');
-            }, 100);
-        }
-    });
-});
-
-const closeModalBtn = document.getElementById('modal-close');
-if (closeModalBtn) {
-    closeModalBtn.addEventListener('click', () => {
-        const modal = document.getElementById('modal');
-        if (modal) {
-            modal.classList.remove('opacity-100');
-            modal.classList.add('opacity-0');
-            setTimeout(() => {
-                modal.classList.replace('flex', 'hidden');
-            }, 150);
-        }
-    });
+    const modal = document.getElementById('modal');
+    if (modal) {
+        modal.classList.replace('hidden', 'flex');
+        setTimeout(() => {
+            modal.classList.replace('opacity-0', 'opacity-100');
+        }, 100);
+    }
 }
+
+function closeModal() {
+    const modal = document.getElementById('modal');
+    const modalImage = document.getElementById('modal-image');
+
+    if (modal) {
+        modal.classList.replace('opacity-100', 'opacity-0');
+        setTimeout(() => {
+            modal.classList.replace('flex', 'hidden');
+        }, 150);
+    }
+
+    if (modalImage) {
+        setTimeout(() => {
+            modalImage.src = '/';
+            modalImage.alt = '';
+        }, 300);
+    }
+}
+
+document.addEventListener('click', (e) => {
+    const target = e.target;
+
+    if (target.matches('#modal-btn')) {
+        openModal(target);
+    } else if (target.matches('#modal-close')) {
+        closeModal();
+    }
+});
