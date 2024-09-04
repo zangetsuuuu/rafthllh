4117e66116a671dac9fa7b655a4085beb35b4e51
Rafif Athallah <rafifathallah99@gmail.com>
2024-09-04 23:41:55 +0700

    feat: add new icons, data, and changes in script

---
+++ src/data/icons.js
@@ -0,0 +1,7 @@
+export const icons = [
+    { name: 'figma', alt: 'Figma icon' },
+    { name: 'tailwindcss', alt: 'Tailwindcss icon' },
+    { name: 'react', alt: 'React icon' },
+    { name: 'astro', alt: 'Astro icon' },
+    { name: 'mysql', alt: 'MySQL icon' },
+];
---
+++ src/scripts/app.js
@@ -0,0 +1,76 @@
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('nav');
+    const info = document.getElementById('more-info');
+    const isAtTop = window.scrollY === 0;
+
+    nav.classList.toggle('translate-y-16', isAtTop);
+    if (info && isAtTop) {
+        info.classList.add('translate-y-60');
+    }
+});
+
+// Modal functions
+function openModal(target) {
+    const targetSrc = target.getAttribute('design-id');
+    const targetTitle = target.getAttribute('design-title');
+
+    if (!targetSrc || !targetTitle) return;
+
+    const modalImage = document.getElementById('modal-image');
+    const modal = document.getElementById('modal');
+    const nav = document.querySelector('nav');
+
+    if (modalImage) {
+        modalImage.src = `/images/gallery/${targetSrc}.webp`;
+        modalImage.alt = targetTitle;
+    }
+
+    if (modal) {
+        modal.classList.replace('hidden', 'flex');
+        setTimeout(() => {
+            modal.classList.replace('opacity-0', 'opacity-100');
+            nav.classList.replace('opacity-100', 'opacity-0');
+        }, 100);
+    }
+}
+
+function closeModal() {
+    const modal = document.getElementById('modal');
+    const modalImage = document.getElementById('modal-image');
+    const nav = document.querySelector('nav');
+
+    if (modal) {
+        modal.classList.replace('opacity-100', 'opacity-0');
+        nav.classList.replace('opacity-0', 'opacity-100');
+        setTimeout(() => {
+            modal.classList.replace('flex', 'hidden');
+        }, 150);
+    }
+
+    if (modalImage) {
+        setTimeout(() => {
+            modalImage.src = '/';
+            modalImage.alt = '';
+        }, 300);
+    }
+}
+
+// More info
+function toggleInfo() {
+    const info = document.getElementById('more-info');
+    if (info) {
+        info.classList.toggle('translate-y-60');
+    }
+}
+
+document.addEventListener('click', (e) => {
+    const { id } = e.target;
+
+    if (id === 'modal-btn') {
+        openModal(e.target);
+    } else if (id === 'modal-close') {
+        closeModal();
+    } else if (id === 'more-icon') {
+        toggleInfo();
+    }
+});
